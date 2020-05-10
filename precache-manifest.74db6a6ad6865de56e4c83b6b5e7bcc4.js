052aeb8cd4422743285aec46db79ec79f3bae561
huynhdh <huynhdh@minerva.vn>
2020-05-10 20:10:54 +0700

    Updates

---
+++ precache-manifest.74db6a6ad6865de56e4c83b6b5e7bcc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1649112ff70e6b0952cc04f186cc10",
+    "url": "/huynhdh/index.html"
+  },
+  {
+    "revision": "db31d4de5c7b0e61f403",
+    "url": "/huynhdh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5f6ba20fe8de5fdf1e3",
+    "url": "/huynhdh/static/js/2.645cb409.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/huynhdh/static/js/2.645cb409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db31d4de5c7b0e61f403",
+    "url": "/huynhdh/static/js/main.0e5ba313.chunk.js"
+  },
+  {
+    "revision": "bd1f14bee4fd2cb5d04f",
+    "url": "/huynhdh/static/js/runtime-main.d352cde5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/huynhdh/static/media/logo.5d5d9eef.svg"
+  }
+]);
